a7dc2005cd3ca2e5a4c8e0b016dc71c547d8bce4
web-mrAlderson <tima-999@inbox.ru>
2020-02-25 14:24:54 +0300

    feat: function max,min,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr = [0]) {
+  if (arr.length != 0) {
+    return Math.min.apply(null, arr);
+  }
+  else {
+    return arr = 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(ar = [0]) {
+
+  if (ar.length != 0) {
+    return Math.max.apply(null, ar);
+  }
+  else {
+    return ar = 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(a = [0]) {
+  if (a.length != 0) {
+    let sum = a.reduce((a, b) => a + b, 0);
+     let result = sum / a.length;
+     return result;
+  }
+  else {
+    return a = 0;
+  }
 }
